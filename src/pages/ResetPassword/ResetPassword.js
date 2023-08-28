0096510c614958ae2e7d5e3f72ffe7281ba60398
bagel.r <Bagel.R@citilink.ru>
2023-08-28 15:37:12 +0300

    add resetPassword logic

--- src/pages/ResetPassword/ResetPassword.js
+++ src/pages/ResetPassword/ResetPassword.js
@@ -1,26 +1,60 @@
-import AuthButtons from '../../components/AuthButtons/AuthButtons';
 import { Button, Input, PasswordInput } from '@ya.praktikum/react-developer-burger-ui-components';
+
+// hooks
+import useForm from '../../hooks/useForm';
+
+// utils
+import { checkResponse } from '../../utils/getUrl';
+// components
+
+import AuthButtons from '../../components/AuthButtons/AuthButtons';
+
+// styles
 import styles from './resetPassword.module.css';
 
 const ResetPassword = () => {
+    const [form, handleForm] = useForm({
+        password: "",
+        token: "",
+    })
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const headers = { 'Content-Type': 'application/json' }
+        const body = JSON.stringify(form);
+
+        fetch('https://norma.nomoreparties.space/api/password-reset/reset', { method: 'POST', headers, body })
+            .then(checkResponse)
+            .then((res) => console.log(res))
+            .catch((error) => console.log(error))
+    }
+
+
     return (
         <main className='content'>
             <h2>
                 Восстановление пароля
             </h2>
-            <form className={styles.form}>
+            <form className={styles.form}
+                onSubmit={handleSubmit}
+            >
                 <PasswordInput
-                    name={'password'}
+                    name='password'
                     placeholder="Введите новый пароль"
                     autoFocus
+                    value={form.password}
+                    onChange={handleForm}
                 />
 
                 <Input
                     type="text"
                     placeholder="Введите код из письма"
-                    name="name"
+                    name="token"
                     errorText="Ошибка"
                     size="default"
+                    value={form.token}
+                    onChange={handleForm}
                 />
 
                 <Button htmlType="submit" type="primary" size="large">
