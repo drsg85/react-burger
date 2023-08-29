aa7ce2ffd990909ebe092922bd10dd543409a70b
bagel.r <Bagel.R@citilink.ru>
2023-08-29 17:01:52 +0300

    add logic to login page and register page refactoring

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -3,32 +3,73 @@ import {
   EmailInput,
   PasswordInput,
 } from '@ya.praktikum/react-developer-burger-ui-components'
+
+import { useNavigate } from 'react-router-dom'
+
+// components
 import AuthButtons from '../../components/AuthButtons/AuthButtons'
+
+// hooks
+import useForm from '../../hooks/useForm'
+
+// styles
 import styles from './login.module.css'
+import { checkResponse } from '../../utils/getUrl'
 
 const Login = () => {
+  const navigate = useNavigate()
+
+  const [form, handleForm] = useForm({
+    email: '',
+    password: '',
+  })
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    const headers = { 'Content-Type': 'application/json' }
+    const body = JSON.stringify(form)
+
+    fetch('https://norma.nomoreparties.space/api/auth/login', {
+      method: 'POST',
+      headers,
+      body,
+    })
+      .then(checkResponse)
+      .then((res) => navigate('/'))
+      .catch((error) => console.log(error))
+  }
+
   return (
     <main className="content">
       <h2>Вход</h2>
-      <form className={styles.form}>
+
+      <form className={styles.form} onSubmit={handleSubmit}>
         <EmailInput
           name="email"
           placeholder="E-mail"
-          // value='email'
+          value={form.email}
+          onChange={handleForm}
         />
+
         <PasswordInput
-        // value='password'
+          name="password"
+          value={form.password}
+          onChange={handleForm}
         />
+
         <Button htmlType="submit" type="primary" size="large">
           Войти
         </Button>
       </form>
+
       <div className="">
         <AuthButtons
           title="Вы — новый пользователь?"
           buttonName="Зарегистрироваться"
           path="/register"
         />
+
         <AuthButtons
           title="Забыли пароль?"
           buttonName="Восстановить пароль"
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,84 +1,89 @@
-import { useNavigate } from 'react-router-dom';
-import { Button, Input, EmailInput, PasswordInput } from '@ya.praktikum/react-developer-burger-ui-components';
+import { useNavigate } from 'react-router-dom'
+import {
+  Button,
+  Input,
+  EmailInput,
+  PasswordInput,
+} from '@ya.praktikum/react-developer-burger-ui-components'
 
 // utils
-import { checkResponse } from '../../utils/getUrl';
+import { checkResponse } from '../../utils/getUrl'
 
 // hooks
-import useForm from '../../hooks/useForm';
+import useForm from '../../hooks/useForm'
 
 // components
-import AuthButtons from '../../components/AuthButtons/AuthButtons';
+import AuthButtons from '../../components/AuthButtons/AuthButtons'
 
 // styles
-import styles from './register.module.css';
+import styles from './register.module.css'
 
 const Register = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate()
 
-    const [form, handleForm] = useForm({
-        name: "",
-        email: "",
-        password: "",
+  const [form, handleForm] = useForm({
+    name: '',
+    email: '',
+    password: '',
+  })
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    const headers = { 'Content-Type': 'application/json' }
+    const body = JSON.stringify(form)
+
+    fetch('https://norma.nomoreparties.space/api/auth/register', {
+      method: 'POST',
+      headers,
+      body,
     })
+      .then(checkResponse)
+      .then((res) => navigate('/'))
+      .catch((error) => console.log(error))
+  }
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
+  return (
+    <main className="content">
+      <h2>Регистрация</h2>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <Input
+          type="text"
+          placeholder="Имя"
+          name="name"
+          errorText="Ошибка"
+          size="default"
+          autoFocus
+          value={form.name}
+          onChange={handleForm}
+        />
 
-        const headers = { 'Content-Type': 'application/json' }
-        const body = JSON.stringify(form);
+        <EmailInput
+          name="email"
+          placeholder="E-mail"
+          value={form.email}
+          onChange={handleForm}
+        />
 
-        fetch('https://norma.nomoreparties.space/api/auth/register', { method: 'POST', headers, body })
-            .then(checkResponse)
-            .then((res) => navigate('/'))
-            .catch((error) => console.log(error))
-    }
+        <PasswordInput
+          name="password"
+          value={form.password}
+          onChange={handleForm}
+        />
 
-    return (
-        <main className='content'>
-            <h2>
-                Регистрация
-            </h2>
-            <form className={styles.form}
-                onSubmit={handleSubmit}
-            >
-                <Input
-                    type="text"
-                    placeholder="Имя"
-                    name="name"
-                    errorText="Ошибка"
-                    size="default"
-                    autoFocus
-                    value={form.name}
-                    onChange={handleForm}
-                />
-
-                <EmailInput
-                    name="email"
-                    placeholder="E-mail"
-                    value={form.email}
-                    onChange={handleForm}
-                />
-
-                <PasswordInput
-                    name='password'
-                    value={form.password}
-                    onChange={handleForm}
-                />
-
-                <Button htmlType="submit" type="primary" size="large">
-                    Зарегистрироваться
-                </Button>
-            </form>
-            <div className="auth__links">
-                <AuthButtons
-                    title="Уже зарегистрированы?"
-                    buttonName="Войти"
-                    path='/login'
-                />
-            </div>
-        </main>
-    )
+        <Button htmlType="submit" type="primary" size="large">
+          Зарегистрироваться
+        </Button>
+      </form>
+      <div className="auth__links">
+        <AuthButtons
+          title="Уже зарегистрированы?"
+          buttonName="Войти"
+          path="/login"
+        />
+      </div>
+    </main>
+  )
 }
 
-export default Register;
+export default Register
