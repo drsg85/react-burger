a567ba0c4d3c53b22e9f4afd80874afcd2892a61
bagel.r <Bagel.R@citilink.ru>
2023-08-31 11:41:09 +0300

    update components

--- src/components/ProfileInfo/ProfileInfo.js
+++ src/components/ProfileInfo/ProfileInfo.js
@@ -2,13 +2,42 @@ import {
   EmailInput,
   Input,
   PasswordInput,
+  Button,
 } from '@ya.praktikum/react-developer-burger-ui-components'
 
+// Redux
+import { useDispatch, useSelector } from 'react-redux'
+
+// Hooks
+import useForm from '../../hooks/useForm'
+
+// styles
 import styles from './profileInfo.module.css'
+import { handleSetUser } from '../../redux/actions/authActions'
 
 const ProfileInfo = () => {
+  const dispatch = useDispatch()
+  const user = useSelector((store) => store.auth.user)
+
+  const initialForm = {
+    name: user ? user.name : '',
+    email: user?.email || '',
+    password: '',
+  }
+
+  const [form, handleForm, resetForm] = useForm(initialForm)
+
+  // isFormEdited
+  const isFormEdited = JSON.stringify(initialForm) !== JSON.stringify(form)
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    dispatch(handleSetUser(form))
+  }
+
   return (
-    <form className={styles.container}>
+    <form className={styles.container} onSubmit={handleSubmit}>
       <Input
         type="text"
         placeholder="Имя"
@@ -16,11 +45,40 @@ const ProfileInfo = () => {
         errorText="Ошибка"
         size="default"
         icon="EditIcon"
+        value={form.name}
+        onChange={handleForm}
       />
 
-      <EmailInput name="email" placeholder="E-mail" />
+      <EmailInput
+        name="email"
+        placeholder="E-mail"
+        value={form.email}
+        onChange={handleForm}
+      />
 
-      <PasswordInput name={'password'} icon="EditIcon" />
+      <PasswordInput
+        name="password"
+        icon="EditIcon"
+        value={form.password}
+        onChange={handleForm}
+      />
+
+      {isFormEdited && (
+        <div className={styles.buttons}>
+          <Button
+            htmlType="button"
+            type="secondary"
+            size="medium"
+            onClick={resetForm}
+          >
+            Отмена
+          </Button>
+
+          <Button htmlType="submit" type="primary" size="medium">
+            Сохранить
+          </Button>
+        </div>
+      )}
     </form>
   )
 }
